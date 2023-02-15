b4cd836eb260bdf615467c665879d8eeab6be2af
WiecznyX <bmunkhtushig00@gmail.com>
2023-02-15 23:37:44 +0800

    header static done

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -5,21 +5,70 @@ const Header = () => {
     <div className="headerContainer">
       <div className="headerTypeOne">
         <div className="brands">
-          <img src={require("../images/Jumpman_logo.png")} className="logos" alt="#" />
-          <img src={require("../images/Converse-logo.png")} className="logos" alt="#" />
+          <img
+            src={require("../images/Jumpman_logo.png")}
+            className="jordanLogo"
+            alt="#"
+          />
+          <img
+            src={require("../images/Converse-logo.png")}
+            className="converseLogo"
+            alt="#"
+          />
         </div>
         <div className="signIn">
-          <span>Find a Store</span>
-          <hr />
-          <span>Help</span>
-          <hr />
-          <span>Join Us</span>
-          <hr />
-          <span>Sign In</span>
+          <span className="nikeJijigSaaral">Find a Store </span>
+          <span className="nikeJijigSaaral">|</span>
+          <span className="nikeJijigSaaral"> Help </span>
+          <span className="nikeJijigSaaral">|</span>
+          <span className="nikeJijigSaaral"> Join Us </span>
+          <span className="nikeJijigSaaral">|</span>
+          <span className="nikeJijigSaaral"> Sign In</span>
         </div>
       </div>
-      <div className="headerTypeTwo"></div>
-      <div className="headerTypeThree"></div>
+      <div className="headerTypeTwo">
+        <img
+          className="nikeLogoOG"
+          src={require("../images/nikeLogo.png")}
+          alt=""
+        />
+        <div className="searchVerOne">
+          <br />
+          <div className="nikeNavbar">
+            <span className="nikeNavTexts">New & Featured </span>
+            <span className="nikeNavTexts">Men</span>
+            <span className="nikeNavTexts">Women</span>
+            <span className="nikeNavTexts">Kids</span>
+            <span className="nikeNavTexts">Accessories</span>
+            <span className="nikeNavTexts">Sale</span>
+          </div>
+          <div className="nikeSearch">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
+              alt=""
+              className="nikeBagAndFavIcon"
+            />
+            <input placeholder="Search" type="text" id="nikeSearchInput" />
+          </div>
+        </div>
+        <div className="nikeBagAndFav">
+          <img
+            className="nikeBagAndFavIcon"
+            src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+            alt=""
+          />
+          <img
+            className="nikeBagAndFavIcon"
+            src="https://cdn-icons-png.flaticon.com/512/2662/2662503.png"
+            alt="https://cdn-icons-png.flaticon.com/512/54/54481.png"
+          />
+        </div>
+      </div>
+      <div className="headerTypeThree">
+        <p className="bannerOne">Free Shipping + Returns, Free Membership, Exclusive Products</p>
+        <p className="bannerTwo">Save Up to 40%</p>
+        <p className="bannerThree">Shop All Our New Markdowns</p>
+      </div>
     </div>
   );
 };
