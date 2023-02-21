a4455d90297dc0dfb7edc899cd15be84275f7681
Dio Brando <diobrando@Dios-MacBook-Pro.local>
2023-02-21 22:19:59 +0800

    SignIn BE FE holbov

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import axios from 'axios';
 import Home from './pages/Home';
 import Signin from './pages/SignIn';
+import UserHome from './pages/UserHome';
+
+export const instance = axios.create({
+  baseURL: 'http://localhost:2000',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
 
 function App() {
   return (
@@ -9,7 +17,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/SignIn" element={<Signin />}/>
+          <Route path="/SignIn" element={<Signin />} />
+          <Route path="/:id" element={<UserHome />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ front/src/pages/UserHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserHome() {
+  return <div>Hello guys its logged site</div>;
+}
+
+export default UserHome;
