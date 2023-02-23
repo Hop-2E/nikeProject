04d46b97c03414877ea3d8e9586aaca7f0fadd95
Dio Brando <diobrando@Dios-MacBook-Pro.local>
2023-02-23 19:25:26 +0800

    pzdaaaa

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+
 import Home from './pages/Home';
-import Signin from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import UserHome from './pages/UserHome';
+import JordanHome from './pages/JordanHome';
+import SignUp from './pages/SignUp';
 
 export const instance = axios.create({
   baseURL: 'http://localhost:2000',
@@ -18,9 +20,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/SignIn" element={<Signin />} />
+          <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/:id" element={<UserHome />} />
+          <Route path="/jordan" element={<JordanHome />} />
         </Routes>
       </BrowserRouter>
     </>
