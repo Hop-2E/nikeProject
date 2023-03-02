11c1858ba93d84a59ab14b6320372b6fdc919a7f
Munkhtushig <munkhtushig9911@lab-1-02s-iMac.local>
2023-03-02 17:23:49 +0800

    product oruulmaar bn

--- front/src/pages/Products.jsx
+++ front/src/pages/Products.jsx
@@ -8,9 +8,13 @@ import Footer from "../components/Footer";
 const Products = () => {
   const [product, setProduct] = useState();
   const getProduct = async () => {
-    const res = await instance.get("/products");
-    // setProduct(res)
-    console.log(res);
+    const res = await instance.get("/product");
+    setProduct(
+      res.data.data.map((el) => {
+        console.log(el);
+        return el;
+      })
+    );
   };
 
   useEffect(() => {
@@ -21,18 +25,25 @@ const Products = () => {
     <>
       <div className="productsContainer">
         <Header />
-        <div>
+        <div className="">
           <div className="productsLeftNav">
             <div className="choose">
               <div className="genderEquality">
-                <p>Gender</p>
-                <img src={require("../images/down.png")} alt="" />
+                <p className="genders">Gender</p>
+                <img
+                  className="icons"
+                  src={require("../images/down.png")}
+                  alt=""
+                />
               </div>
             </div>
-            <div></div>
-            <div></div>
           </div>
-          <div className="productsRightNav"></div>
+          <div className="productsRightNav">
+            {product && 
+              product.map((el) => {
+                return 
+              })}
+          </div>
         </div>
         <Footer />
       </div>
