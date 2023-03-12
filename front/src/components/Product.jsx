aeba31401261f97fd7dc1027a754bd046f45f7fe
Barsaa75 <110914653+Barsaa75@users.noreply.github.com>
2023-03-12 21:47:44 +0800

    Productaa avchirdg bolsn baga zereg enhance hrgtei

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,9 +1,8 @@
 import "../App.css";
 
 const Product = ({ el }) => {
-  console.log(el) // user_id orj ired bhi
   return (
-    <div className="productContainer">
+    <div className="productContainer" key={Math.random()} >
       <div className="pruducts">
         <img className="productImage" src={el.picture} alt="" />
         <div className="infoProduct">
@@ -11,9 +10,6 @@ const Product = ({ el }) => {
           <span className="grayProductText">{el.type}</span>
           <span className="grayProductText">{el.color}</span>
           <span className="productTitle">{el.price}Price</span>
-          <span className="productTitle">{el.user_id}</span>
-          <span className="productTitle">{el.productId}</span>
-          <span className="productTitle">{el}</span>
         </div>
       </div>
     </div>
